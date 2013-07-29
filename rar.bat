f06a72f7064ecd25e85b03f8414215153cbdcb38
wongshan07@gmail.com <wongshan07@gmail.com@5aa49f5c-d131-211b-44b0-0062081ef677>
2013-07-29 13:10:23 +0000

    修改压缩工具为7z

--- rar.bat
+++ rar.bat
@@ -7,16 +7,15 @@ set bin=%pro%_PCB01_gprs_MT6252_S01.MAUI_10A_W11_08_MP_V3.bin
 set sct=scat%pro%.txt
 set dbg=tst\database_classb\BPLGUInfoCustomAppSrcP_MT6252_S01_MAUI_10A_W11_08_MP_V3
 
-set rexe="C:\Program Files\WinRAR\Rar.exe"
-set rar=%date:~5,2%%date:~8,2%_%user_pro%_v%time:~0,2%%time:~3,2%.rar
+set rexe="C:\Program Files\7-Zip\7z.exe"
+set rar=%date:~5,2%%date:~8,2%_%user_pro%_v%time:~0,2%%time:~3,2%.7z
 set opath=build\%pro%\
 
 if not exist build\%pro% md \build\%pro% 
 if not exist %opath%%bin% echo BIN文件不存在
 if not exist %opath%%sct% echo SCT文件不存在
 if not exist %dbg% echo DBG文件不存在
-
-call %rexe% -ep a %opath%%rar% %opath%%bin% %opath%%sct% %dbg%
+call %rexe% a -t7z -mx9 %opath%%rar% %opath%%bin% %opath%%sct% %dbg%
 
 if errorlevel 0 echo 压缩成功
 if errorlevel 1 echo 压缩失败，请手动压缩
